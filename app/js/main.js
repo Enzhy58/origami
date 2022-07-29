4f144f9704910d755888b48d20d006b43c53afd6
Enzhy58 <ang.borisova2015@yandex.ru>
2022-07-30 00:13:48 +0300

    added menu page, added breadcrumbs, added catalog section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,70 @@
 $(function(){
+  var $range = $(".filter-price__input");
+  var $inputFrom = $(".filter-price__from");
+  var $inputTo = $(".filter-price__to");
+  var instance;
+  var min = 0;
+  var max = 1200;
+  var from = 0;
+  var to = 0;
+
+  $range.ionRangeSlider({
+    skin: "round",
+    type: "double",
+    min: min,
+    max: max,
+    from: 200,
+    to: 800,
+    onStart: updateInputs,
+    onChange: updateInputs,
+    onFinish: updateInputs
+  });
+  instance = $range.data("ionRangeSlider");
+
+  function updateInputs(data) {
+    from = data.from;
+    to = data.to;
+
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);
+  }
+
+  $inputFrom.on("change", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < min) {
+      val = min;
+    } else if (val > to) {
+      val = to;
+    }
+
+    instance.update({
+      from: val
+    });
+
+    $(this).prop("value", val);
+
+  });
+
+  $inputTo.on("change", function () {
+    var val = $(this).prop("value");
+
+    // validate
+    if (val < from) {
+      val = from;
+    } else if (val > max) {
+      val = max;
+    }
+
+    instance.update({
+      to: val
+    });
+
+    $(this).prop("value", val);
+  });
+
+  $('.select-style').styler();
+
   var mixer = mixitup('.categories__list');
 });
