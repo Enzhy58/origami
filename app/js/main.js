b7d6aeeee8cff0dff41296e53273627a5ea389a9
Enzhy58 <ang.borisova2015@yandex.ru>
2023-09-03 13:48:16 +0300

    added counter

--- app/js/main.js
+++ app/js/main.js
@@ -120,6 +120,21 @@ $(function () {
     readOnly: true
   });
 
+  $('.reviews__slider').slick({
+    dots: true,
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg width="40" height="40" viewBox="0 0 40 40" fill="none"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke ="#C2C2C2" stroke-width="3"/><path d="M21.1968 29.0492L13.4508 21.3033C12.8497 20.699 12.8497 19.7231 13.4508 19.1189L21.1968 11.3729C21.7761 10.8757 22.6328 10.8757 23.2122 11.3729C23.8614 11.9291 23.9372 12.9082 23.3811 13.5573L16.7351 20.2033L23.3811 26.8648C23.9822 27.469 23.9822 28.445 23.3811 29.0492C22.7769 29.6503 21.8009 29.6503 21.1968 29.0492Z" fill="#C2C2C2"/></svg ></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="40" height="40" viewBox="0 0 40 40" fill="none"><circle cx="20" cy="20" r="18.5" fill="white" fill-opacity="0.01" stroke ="#C2C2C2" stroke-width="3"/><path d="M18.6352 11.4508L26.3811 19.1967C26.9822 19.801 26.9822 20.7769 26.3811 21.3811L18.6352 29.1271C18.0558 29.6243 17.1991 29.6243 16.6197 29.1271C15.9706 28.5709 15.8947 27.5918 16.4509 26.9427L23.0968 20.2967L16.4508 13.6352C15.8497 13.031 15.8497 12.055 16.4508 11.4508C17.055 10.8497 18.031 10.8497 18.6352 11.4508Z" fill="#C2C2C2"/></svg ></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false
+        }
+      }
+    ]
+  });
+
   $('.interest__list').slick({
     dots: false,
     slidesToShow: 5,
@@ -225,19 +240,38 @@ btns.forEach(btn => {
 });
 
 
-let switchMode = document.getElementById("switch");
-
-switchMode.onclick = function () {
-  let theme = document.getElementById("theme");
 
 
-  if (theme.getAttribute("href") == "#") {
-    theme.href = "css/dark-mode.min.css"; 
-  } else {
-    theme.href = "#";
-  }
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]');
 
-};
+
+btnMinus.addEventListener('click', function(){
+  if (parseInt(counter.innerText) > 1) {
+    counter.innerText = --counter.innerText;
+  };
+});
+
+btnPlus.addEventListener('click', function (){
+  counter.innerText = ++counter.innerText;
+});
+
+
+window.addEventListener('click', function (event) {
+  if (event.target.dataset.action === 'plus') {
+    const counterWrapper = event.target.closest('.counter-wrapper');
+    const counter = counterWrapper.querySelector('[data-counter]');
+    counter.innerText = ++counter.innerText;
+  };
+  if (event.target.dataset.action === 'minus') {
+    const counterWrapper = event.target.closest('.counter-wrapper');
+    const counter = counterWrapper.querySelector('[data-counter]');
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText;
+    };
+  };
+});
 
 const cartList = document.querySelector('.cart__list');
 window.addEventListener('click', function (event) {
@@ -248,21 +282,29 @@ window.addEventListener('click', function (event) {
       imgSrc: productCard.querySelector('.product-card__img').getAttribute('src'),
       name: productCard.querySelector('.product-card__name').innerText,
       price: productCard.querySelector('.product-card__price').innerText,
+      counter: productCard.querySelector('[data-counter]').innerText,
 
     };
     const cartItemHtml = `
-    <li class="cart__item" data-id=${productInfo.id}>
-          <a class="cart__link" href="#">
-            <img class="cart__img" src="${productInfo.imgSrc}" width="108" height="72" alt="ролл белый тигр">
-            <div class="cart__box">
-              <h3 class="cart__name">${productInfo.name}</h3>
+      <li class="cart__item" data-id=${productInfo.id}>
+        <img class="cart__img" src="${productInfo.imgSrc}" width="100" height="65" alt="ролл белый тигр">
+        <div class="cart__box">
+          <h3 class="cart__name">${productInfo.name}</h3>
+          <div class="cart__wrapper">
+              <div class="counter">
+                <button class="counter__control" data-action="minus">-</button>
+                <div class="counter__current" data-counter="">${productInfo.counter}</div>
+                <button class="counter__control" data-action="plus">+</button>
+              </div>
               <span class="cart__price">${productInfo.price}</span>
             </div>
-            <svg class="cart__icon">
-              <use xlink:href="images/sprite.svg#close-filters"></use>
-            </svg>
-          </a>
-        </li>`;
-    cartList.insertAdjacentHTML('beforeend', cartItemHtml);    
+        </div>
+        <button class="cart__delite" type="submit">
+          <svg class="cart__icon">
+            <use xlink:href="images/sprite.svg#close-filters"></use>
+          </svg>
+        </button>
+      </li>`;
+    cartList.insertAdjacentHTML('beforeend', cartItemHtml);
   }
 })
